6eee5c84706a7107193b4cd2864239011d8990ba
Stjepovic <stjepovicmatej2@gmail.com>
2021-02-08 23:23:30 +0100

    Update na curiculum stranici
    Dohvacanje podataka o pojedinom kolegiju

--- scripts/curiculum.js
+++ scripts/curiculum.js
@@ -1,34 +1,36 @@
 
-var data="";
 const cur=new XMLHttpRequest();
-
-
 cur.open('get','http://www.fulek.com/VUA/SUPIT/GetNastavniPlan');
 cur.onreadystatechange = function () {
     if (cur.readyState == 4 ) {
          initAutocomplete (this.responseText);
-      
-      }
-      
+      }  
   }
 cur.send();
- console.log(availableTags);
-  $( "#tags" ).autocomplete({
-    source: availableTags
-  });
- 
-
 function initAutocomplete (data){
-    var availableTags = JSON.parse(data);
-    console.log(availableTags);
-    
+    var availableTags = JSON.parse(data);    
     $("#tags").autocomplete({
         source: availableTags,
         select: function( event, ui ) { 
             event.preventDefault();
             $('#tags').val(ui.item.label);
-            console.log(ui.item);
+            GetCuriculum(ui.item.value)
         }   
       });
+}
+function GetCuriculum(value){
+        const cur=new XMLHttpRequest();
+            cur.open('get','http://www.fulek.com/VUA/SUPIT/GetKolegij/'+value);
+            cur.onreadystatechange = function () {
+    if (cur.readyState == 4 ) {
+         var kolegij=JSON.parse(this.responseText)
+         AddTableItem(kolegij);
+      }  
+  }
+cur.send();
+}
+
+function AddTableItem(value){
+    
 }
 
