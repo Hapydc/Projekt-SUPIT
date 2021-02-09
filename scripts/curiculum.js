764fb53d73646ad918b87fde5682bfbacd9ebcf6
Stjepovic <stjepovicmatej2@gmail.com>
2021-02-09 19:31:03 +0100

    Delete button
    Napravljen delete button koji briše zadani tr

--- scripts/curiculum.js
+++ scripts/curiculum.js
@@ -26,31 +26,39 @@ function GetCuriculum(value){
     if (cur.readyState == 4 ) {
          var kolegij=JSON.parse(this.responseText)
          AddTableItem(kolegij);
+         AddToArray(kolegijArray,kolegij.id,kolegij.ects,kolegij.hours);
          Sum(kolegij.ects,kolegij.sati);
-         CreateTableHeader();
-         
       }  
   }
 cur.send();
 }
-
-
 function AddTableItem(value){
   console.log(value);
   
-  $('#myTable').append('<tr><td>'+value.kolegij+'</td><td>'+value.ects+'</td><td>'+value.sati+'</td><td>'+value.predavanja+'</td><td>'+value.vjezbe+' </td><td>'+value.tip+'</td></tr>');
+  $('#myTable').append('<tr><td>'+value.kolegij+'</td><td>'+value.ects+'</td><td>'+value.sati+'</td><td>'+value.predavanja+'</td><td>'+value.vjezbe+' </td><td>'+value.tip+'</td><td><button onclick=DeleteRow(this.parentNode.parentNode.rowIndex) class="btn">Obriši</button></td></tr>');
 } 
-
 var ects=0;
 var hours=0;
 function Sum(a,b){
   ects =ects+ a;
   hours =hours+b;
-
   console.log(ects, hours);
   $('.footerRow').remove();
   $('#myTable').append('<tr class="footerRow"><td>Ukupno</td><td>'+ects+'</td><td>'+hours+'</td></tr>');
 }
+function AddToArray(array,id,ects,hours){
+array.push(id,ects,hours);
+console.log(array);
+}
+var kolegijArray =[
+];
+
+function DeleteRow(i){
+  document.getElementById('myTable').deleteRow(i)
+}
+
+
+
 
 
 
